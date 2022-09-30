d5a8ceebd47dd41591805d4560404536f2300eb6
AliCelikay <111723837+AliCelikay@users.noreply.github.com>
2022-09-30 18:38:54 -0500

    Bug fixes in employee class files

--- index.js
+++ index.js
@@ -48,6 +48,7 @@ let firstQuestion = () => {
     ])
 }
 
+//Employee queries and calling another employee function to roster
 let managerQueries = () => {
     inquirer
     .prompt([
@@ -73,6 +74,7 @@ let managerQueries = () => {
         },
     ]).then((ans) => {
         managerCardInfo(ans);
+        init();
     })
 }
 
@@ -135,7 +137,7 @@ let internQueries = () => {
 
 
 
-
+// Employee info and rendering card information functions
 const managerCardInfo = (ans) => {
     let {name, id, email, officeNumber} = ans;
     let manager = new Manager(name, id, email, officeNumber);
@@ -143,14 +145,14 @@ const managerCardInfo = (ans) => {
 }
 
 const engineerCardInfo = (ans) => {
-    let {name, id, email, officeNumber} = ans;
-    let engineer = new Engineer(name, id, email, officeNumber);
+    let {name, id, email, github} = ans;
+    let engineer = new Engineer(name, id, email, github);
     fs.appendFile('./dist/index.html', engineer.renderCard() , (err) => err ? console.error(err) : console.log('Success!'));
 }
 
 const internCardInfo = (ans) => {
-    let {name, id, email, officeNumber} = ans;
-    let intern = new intern(name, id, email, officeNumber);
+    let {name, id, email, school} = ans;
+    let intern = new Intern(name, id, email, school);
     fs.writeFile('./dist/index.html', intern.renderCard() , (err) => err ? console.error(err) : console.log('Success!'));
 }
 
@@ -158,103 +160,5 @@ const internCardInfo = (ans) => {
 
 
 
-
-// const init = () => {
-//     inquirer
-//         .prompt([
-//             {
-//                 type: 'list',
-//                 message: 'Please choose your job title:',
-//                 choices: ['Manager', 'Engineer', 'Intern', 'Exit'],
-//                 name: 'firstChoice',
-//             }
-//         ])
-//         .then((ans) => {
-//             if(ans.firstChoice === "Manager"){
-//                 inquirer
-//                 .prompt([
-//                     {
-//                     type: 'input',
-//                     message: 'Please enter your name:',
-//                     name: 'managerName',
-//                     },
-//                     {
-//                         type: 'input',
-//                         message: 'Please enter your employee id:',
-//                         name: 'managerID',
-//                     },
-//                     {
-//                         type: 'input',
-//                         message: 'Please enter your email address:',
-//                         name: 'managerEmail',
-//                     },
-//                     {
-//                         type: 'input',
-//                         message: 'Please enter your office number:',
-//                         name: 'managerOfficeNum',
-//                     },
-//                 ])
-//                 .then((ans) =>{
-//                     console.log(ans);
-//                 })
-//             }
-//             else if(ans.firstChoice === "Engineer"){
-//                 inquirer
-//                 .prompt([
-//                     {
-//                     type: 'input',
-//                     message: 'Please enter your name:',
-//                     name: 'engineerName',
-//                     },
-//                     {
-//                         type: 'input',
-//                         message: 'Please enter your employee id:',
-//                         name: 'enginID',
-//                     },
-//                     {
-//                         type: 'input',
-//                         message: 'Please enter your GitHub username:',
-//                         name: 'enginGitHub',
-//                     },
-//                 ])
-//                 .then((ans) =>{
-//                     console.log(ans);
-//                 })
-//             }
-//             else if(ans.firstChoice === "Intern"){
-//                 inquirer
-//                 .prompt([
-//                     {
-//                     type: 'input',
-//                     message: 'Please enter your name:',
-//                     name: 'internName',
-//                     },
-//                     {
-//                         type: 'input',
-//                         message: 'Please enter your employee id:',
-//                         name: 'internID',
-//                     },
-//                     {
-//                         type: 'input',
-//                         message: 'Please enter your email address:',
-//                         name: 'internEmail',
-//                     },
-//                     {
-//                         type: 'input',
-//                         message: 'Please enter your graduate school:',
-//                         name: 'internSchool',
-//                     },
-//                 ])
-//                 .then((ans) =>{
-//                     console.log(ans);
-//                 })
-//             }
-//             else if(ans.firstChoice === "Exit"){
-//                 console.log(`Thankyou! Your html file is ready`);
-//                 // print html
-//             }
-//         })
-
-// }
-
+//Calling the initial function
 init();
